235423f9f846421c8b4dae2940b7ea2b93fae3e8
jahid oussama <jahid20oussama@gmail.com>
2022-09-08 16:58:51 +0100

    i finished challlenge 8

---
+++ challenge8/challenge8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	float a,b,c,d;
+	
+	 printf("hello,\nthanks for your visiting \n enter 4 numbers : ");
+	 
+	 
+	 scanf("%f %f %f %f",&a,&b,&c,&d);
+	 
+	 printf("here you can see the sum : %.2f + %.2f + %.2f+ %.2f = %.2f \nand here you can see the (Moyenne) : %.2f / %.2f = %.2f",a,b,c,d,a+b+c+d,a+b+c+d,4.00,(a+b+c+d)/4);
+	 
+	 
+	 
+}
