60f8cf2f7b852d5e0f4517b03a11101403dfa223
oussama <jahid20oussama@gmail.com>
2022-09-09 00:36:27 +0200

    modified challenge 8

--- challenge8/challenge8.cpp
+++ challenge8/challenge8.cpp
@@ -9,7 +9,7 @@ int main()
 	 
 	 scanf("%f %f %f %f",&a,&b,&c,&d);
 	 
-	 printf("here you can see the sum : %.2f + %.2f + %.2f+ %.2f = %.2f \nand here you can see the (Moyenne) : %.2f / %.2f = %.2f",a,b,c,d,a+b+c+d,a+b+c+d,4.00,(a+b+c+d)/4);
+	 printf("here you can see the sum : %.2f + %.2f + %.2f+ %.2f = %.2f \nand here you can see the (Moyenne) : %.2f / %d = %.2f",a,b,c,d,a+b+c+d,a+b+c+d,4,(a+b+c+d)/4);
 	 
 	 
 	 
